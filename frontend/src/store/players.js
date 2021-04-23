8756e86e7b17fa8a79973205d899bbd77e2bca5f
Safiya Cain <say.cain3@gmail.com>
2021-04-23 16:41:49 -0400

    add thunk action for getting one player to athleteReducer

--- frontend/src/store/players.js
+++ frontend/src/store/players.js
@@ -1,6 +1,8 @@
 import { fetch } from './csrf';
 
-const GET_PLAYERS = 'GET_PLAERS';
+const GET_PLAYERS = 'GET_PLAYERS';
+
+const GET_ONE_PLAYER = 'GET_ONE_PLAYER';
 
 //POJO ACTION FOR GETTING ALL MLB PLAYERS
 const getAllPlayers = (players) => {
@@ -10,10 +12,24 @@ const getAllPlayers = (players) => {
     }
 }
 
+//POJO ACTION FOR GETTING ONE MLB PLAYER BY ID
+const getOnePlayer = (player) => {
+    return{
+        type: GET_ONE_PLAYER,
+        payload: player
+    }
+}
+
 //THUNK ACTION FOR GETTING ALL OF THE PLAYERS
 export const fetchAllPlayers = () => async(dispatch) => {
     const response = await fetch(`api/athletes`);
     dispatch(getAllPlayers(response.data))
+}
+
+//THUNK ACTION FOR GETTING ONE PLAYER
+export const fetchOnePlayer = (id) => async(dispatch) => {
+    const response = await fetch(`api/athletes/${id}`);
+    dispatch(getOnePlayer(response.data))
 }
 
 const initialState = {};
@@ -27,6 +43,8 @@ const athleteReducer = (state=initialState, action) => {
                allPlayers[player.playerId] = player
            })
            return allPlayers;
+        case GET_ONE_PLAYER:
+            return{...state, [action.payload.id]: action.payload};
         default:
             return state;
     }
