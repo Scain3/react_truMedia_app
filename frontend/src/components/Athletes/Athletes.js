48cc6479903485287d1521494f07e1915fcb6808
Safiya Cain <say.cain3@gmail.com>
2021-04-28 13:00:41 -0400

    style link to all athletes

--- frontend/src/components/Athletes/Athletes.js
+++ frontend/src/components/Athletes/Athletes.js
@@ -15,16 +15,23 @@ function Athletes(){
 
 
     return(
-        <div className="all-players-div">
-            {allPlayers && allPlayers.map((player, index = 0) => (
-                <div className="player-block-data" onClick={()=>history.push(`player/${player.playerId}`)}>
-                    <div className="player-data">{index += 1 }</div>
-                    <div className="player-data player-img"><img src={player.playerImage} alt={player.fullName} /></div>
-                    <div className="player-data player-name">{player.fullName}</div>
-                    <div className="player-data player-id">{player.playerId}</div>
-                    <div className="player-data player-team__img"><img src={player.teamImage} alt={`${player.fullName}'s Team`} /></div>
-                </div>
-            ))}
+        <div className="all-athletes-pg-div">
+            <div className="headers">
+                <div className="header-divs header-head__shot">{"Head Shot"}</div>
+                <div className="header-divs header-full__name">{"Full Name"}</div>
+                <div className="header-divs header-id">{"ID"}</div>
+                <div className="header-divs header-team">{"Team"}</div>
+            </div>
+            <div className="all-players-div">
+                {allPlayers && allPlayers.map((player, index = 0) => (
+                    <div className="player-block-data" onClick={()=>history.push(`player/${player.playerId}`)}>
+                        <div className="player-data player-img__div"><img className="player-img" src={player.playerImage} alt={player.fullName} /></div>
+                        <div className="player-data player-name">{player.fullName}</div>
+                        <div className="player-data player-id">{player.playerId}</div>
+                        <div className="player-data player-team__img"><img src={player.teamImage} alt={`${player.fullName}'s Team`} /></div>
+                    </div>
+                ))}
+            </div>
         </div>
     )
 
