aa346126e036e79c7a2809e4a3f677c567be2b73
Safiya Cain <say.cain3@gmail.com>
2021-04-28 18:37:50 -0400

    add headers to single athlete page

--- frontend/src/components/Athletes/SingleAthlete.js
+++ frontend/src/components/Athletes/SingleAthlete.js
@@ -13,25 +13,48 @@ function SingleAthlete(){
     }, [id, dispatch])
 
     return(
-        <div>
-            <div>{singlePlayer.playerId}</div>
-            <div>{singlePlayer.fullName}</div>
-            <div><img src={singlePlayer.playerImage} alt={singlePlayer.fullName} /></div>
-            <div>{singlePlayer.seasonYear}</div>
-            <div>{singlePlayer.week}</div>
-            <div>{singlePlayer.team}</div>
-            <div><img src={singlePlayer.teamImage} alt={singlePlayer.team} /></div>
-            <div>{singlePlayer.opponent}</div>
-            <div><img src={singlePlayer.opponentImage} alt={singlePlayer.opponent} /></div>
-            <div>{singlePlayer.Att}</div>
-            <div>{singlePlayer.Cmp}</div>
-            <div>{singlePlayer.Sack}</div>
-            <div>{singlePlayer.Int}</div>
-            <div>{singlePlayer.PsYds}</div>
-            <div>{singlePlayer.PsTd}</div>
-            <div>{singlePlayer.Rush}</div>
-            <div>{singlePlayer.RshYds}</div>
-            <div>{singlePlayer.RshTD}</div>
+        <div className="single-athlete-pg-div">
+            {/* <div className="single-athlete-side-header">
+                <div>{"ID"}</div>
+                <div>{"Full Name"}</div>
+                <div>{"Head Shot"}</div>
+                <div>{}</div>
+                <div>{}</div>
+                <div>{}</div>
+                <div>{}</div>
+                <div>{}</div>
+                <div>{}</div>
+                <div>{}</div>
+                <div>{}</div>
+                <div>{}</div>
+                <div>{}</div>
+                <div>{}</div>
+                <div>{}</div>
+                <div>{}</div>
+                <div>{}</div>
+                <div>{}</div>
+                <div>{}</div>
+            </div> */}
+            <div>
+                <div><div>{"ID"}</div>{singlePlayer.playerId}</div>
+                <div><div>{"Player"}</div>{singlePlayer.fullName}</div>
+                <div><div>{"Head Shot"}</div><img src={singlePlayer.playerImage} alt={singlePlayer.fullName} /></div>
+                <div><div>{"Season Year"}</div>{singlePlayer.seasonYear}</div>
+                <div><div>{"Week"}</div>{singlePlayer.week}</div>
+                <div><div>{"Team"}</div>{singlePlayer.team}</div>
+                <div><div>{"Team Image"}</div><img src={singlePlayer.teamImage} alt={singlePlayer.team} /></div>
+                <div><div>{"Opponent"}</div>{singlePlayer.opponent}</div>
+                <div><img src={singlePlayer.opponentImage} alt={singlePlayer.opponent} /></div>
+                <div>{singlePlayer.Att}</div>
+                <div>{singlePlayer.Cmp}</div>
+                <div>{singlePlayer.Sack}</div>
+                <div>{singlePlayer.Int}</div>
+                <div>{singlePlayer.PsYds}</div>
+                <div>{singlePlayer.PsTd}</div>
+                <div>{singlePlayer.Rush}</div>
+                <div>{singlePlayer.RshYds}</div>
+                <div>{singlePlayer.RshTD}</div>
+            </div>
         </div>
     )
 }
