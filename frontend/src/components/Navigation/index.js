d7881f28b473a86be833090418285ef8a782d91f
Safiya Cain <say.cain3@gmail.com>
2021-05-03 12:45:04 -0400

    update navigation component

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -1,32 +1,14 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import { useSelector } from 'react-redux';
-import ProfileButton from './ProfileButton';
-import LoginFormModal from '../LoginFormModal';
 import './Navigation.css';
 
-function Navigation({ isLoaded }){
-  const sessionUser = useSelector(state => state.session.user);
+function Navigation(){
 
-  let sessionLinks;
-  if (sessionUser) {
-    sessionLinks = (
-      <ProfileButton user={sessionUser} />
-    );
-  } else {
-    sessionLinks = (
-      <>
-        <LoginFormModal />
-        <NavLink to="/signup">Sign Up</NavLink>
-      </>
-    );
-  }
 
   return (
     <ul>
       <li>
-        <NavLink exact to="/">Home</NavLink>
-        {isLoaded && sessionLinks}
+        <NavLink exact to="/">TruMedia Networks</NavLink>
       </li>
     </ul>
   );
